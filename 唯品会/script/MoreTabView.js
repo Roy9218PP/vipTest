b97206b072a80c3f02dc3bd0bbedca12d7d6b5a8
roy <zuy89@icloud.com>
2016-08-15 15:19:45 +0800

    roy更多下拉框

--- 唯品会/script/MoreTabView.js
+++ 唯品会/script/MoreTabView.js
@@ -16,9 +16,37 @@ MoreTabView.prototype.load = function(){
 	
 	var left = moreRight -  $('.more-modal').innerWidth() + 'px'
 	
-	$('.more-modal').show().css({
-		
+	$('.more-modal')
+	.show()
+	.css({
 		left:left
+	})
+	.hover(function(){
+		
+		$(this).show()
+		
+	},function(){
+		
+		$(this).hide()
+	})
+	
+	
+	$('.more-modal li').hover(function(){
+		
+		$(this).find('p').stop().animate({
+			
+			top:'0'
+			
+		},300)
+		
+	},function(){
+		
+		$(this).find('p').stop().animate({
+			
+			top:'25%'
+			
+		},300)
+		
 	})
 }
 
